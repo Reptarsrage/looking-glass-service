d2091c21d20fbdc442797cc76e42583fdda3ec91
Justin Robb <reptarsrage@gmail.com>
2022-05-23 23:56:08 -0700

    Improving test

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,21 +1,8 @@
 import tap from "tap";
-import axios from "axios";
+import S from "fluent-json-schema";
+import Ajv from "ajv";
 import { build } from "./app";
-import { AddressInfo } from "net";
-
-tap.test("E2E GET `/` route", async (t) => {
-  t.plan(3);
-
-  const fastify = build();
-  t.teardown(() => fastify.close());
-
-  await fastify.listen(0);
-  const response = await axios.get("http://localhost:" + (fastify.server.address() as AddressInfo).port);
-
-  t.equal(response.status, 200);
-  t.equal(response.headers["content-type"], "application/json; charset=utf-8");
-  t.matchSnapshot(response.data);
-});
+import { ModuleResponseSchema } from "./dto/moduleResponse";
 
 tap.test("GET `/` route", (t) => {
   t.plan(4);
@@ -32,7 +19,11 @@ tap.test("GET `/` route", (t) => {
       t.error(err);
       t.equal(response.statusCode, 200);
       t.equal(response.headers["content-type"], "application/json; charset=utf-8");
-      t.matchSnapshot(response.json());
+
+      const ajv = new Ajv({ allErrors: true });
+      const schema = S.array().items(ModuleResponseSchema);
+      const validate = ajv.compile(schema.valueOf());
+      t.ok(validate(response.json()));
     }
   );
 });
