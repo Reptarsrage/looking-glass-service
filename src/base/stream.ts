70e2d6e2331102b3e7ed73ec6cd76016388e5327
Justin Robb <reptarsrage@gmail.com>
2022-08-13 16:28:19 -0700

    Adding retry to stream

--- src/base/stream.ts
+++ src/base/stream.ts
@@ -2,19 +2,29 @@ import axios, { AxiosRequestHeaders } from "axios";
 import type { FastifyReply } from "fastify";
 import type { Readable } from "node:stream";
 
-export default async function stream(url: string, headers: AxiosRequestHeaders, res: FastifyReply): Promise<void> {
+export default async function stream(
+  url: string,
+  headers: AxiosRequestHeaders,
+  res: FastifyReply,
+  tries = 3
+): Promise<void> {
   try {
     const response = await axios.get<Readable>(url, {
       headers,
       responseType: "stream",
     });
 
-    if (response.status >= 200 && response.status < 300) {
-      response.headers["cache-control"] = "public, max-age=31536000";
+    if (response.status >= 200 && response.status < 400) {
+      response.headers["cache-control"] = "max-age=3600, must-revalidate";
     }
 
     response.data.pipe(res.raw);
   } catch (error: unknown) {
+    if (tries >= 0) {
+      await stream(url, headers, res, tries - 1);
+      return;
+    }
+
     res.send(error);
   }
 }
