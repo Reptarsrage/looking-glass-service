ba7ca6b08b4bdac620e8f14b52761cedcbc122c5
Justin Robb <reptarsrage@gmail.com>
2022-08-13 16:27:37 -0700

    Updating pixiv app version

--- src/pixiv/pixiv.http.ts
+++ src/pixiv/pixiv.http.ts
@@ -4,12 +4,12 @@ const httpService = axios.create({
   baseURL: "https://app-api.pixiv.net/",
   headers: {
     "App-OS": "ios",
-    "App-Version": "7.14.9",
-    "App-OS-Version": "15.4.1",
+    "App-Version": "7.15.5",
+    "App-OS-Version": "15.6",
     "app-accept-language": "en",
     "Accept-Language": "en-us",
     "X-Client-Hash": "db0aa4ab85cefa8d09b1eec558b19d8d",
-    "User-Agent": "PixivIOSApp/7.14.9 (iOS 15.4.1; iPhone14,2)",
+    "User-Agent": "PixivIOSApp/7.15.5 (iOS 15.6; iPhone14,2)",
   },
   timeout: 5000,
 });
