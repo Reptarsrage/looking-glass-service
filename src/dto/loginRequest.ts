7c821cfcaa8f8fd2198ad438ef9e093976d65896
Justin Robb <reptarsrage@gmail.com>
2022-11-13 12:35:36 -0800

    Fixing auth query params for implicit auth requests

--- src/dto/loginRequest.ts
+++ src/dto/loginRequest.ts
@@ -9,5 +9,5 @@ export const LoginRequestSchema = S.object()
   .id("LoginRequest")
   .title("LoginRequest")
   .description("Login Request")
-  .prop("username", S.string().required())
-  .prop("password", S.string().required());
+  .prop("username", S.string()) // Not required for implicit login
+  .prop("password", S.string()); // Not required for implicit login
